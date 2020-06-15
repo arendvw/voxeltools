b2f798a34ce33bbeeb01bc3cc3370ead47d65046
Arend van Waart <arend@studioavw.nl>
2020-06-15 11:45:17 +0200

    feature: boudewijn's component to get the voxelsize and voxel count from a grid

---
+++ VoxelTools/Components/VoxelGrid/VoxelGridDimensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using StudioAvw.Voxels.Geometry;
+using StudioAvw.Voxels.Param;
+
+namespace StudioAvw.Voxels.Components.VoxelGrid
+{
+    /// <summary>
+    /// Enumerate the voxel grid
+    /// </summary>
+    public class VoxelGridDimensions : BaseVoxelComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the VoxelGridIntersect class.
+        /// </summary>
+        public VoxelGridDimensions()
+            : base("VoxelGrid Dimensions", "VGDimensions",
+                "Get the dimensions of a voxelgrid",
+                "Voxels", "Analysis")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new Param_VoxelGrid(), "Grids", "G", "Voxelgrid to analyse", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Voxelsize", "S", "X, Y, Z size of a voxel", GH_ParamAccess.item);
+            pManager.AddPointParameter("Voxelcount", "C", "Amount of voxels in X, Y and Z direction", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var vg = default(VoxelGrid3D);
+            DA.GetData(0, ref vg);
+
+            DA.SetData(0, vg.VoxelSize);
+            DA.SetData(1, vg.SizeUVW.ToPoint3D());
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override Bitmap Icon =>
+            //You can add image files to your project resources and access them like this:
+            // return Resources.IconForThisComponent;
+            Images.VT_Decompose;
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("{3123E7DD-38F8-4050-A31A-03524B21776A}");
+    }
+}
